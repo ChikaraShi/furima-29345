897c21ad17d3b110bfbbc1c0afbeabb3c14283fe
ChikaraShi <chikara.2000.082@gmail.com>
2020-09-09 14:13:01 +0900

    Basic認証の導入

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth
+
+  private
+
+  def basic_auth
+    authenticare_or_request_with_http_basic do |username, passward|
+      username == ENV["BASIC_AUTH_USER"] && passward == ENV["BASIC_AUTH_PASSWARD"]
+    end
+  end
+
 end
