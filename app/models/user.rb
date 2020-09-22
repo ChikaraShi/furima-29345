670d041b959d2f2ebadc3a97343cc1583a21591e
ChikaraShi <chikara.2000.082@gmail.com>
2020-09-22 16:13:57 +0900

    userモデルfix2

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,27 +6,17 @@ class User < ApplicationRecord
   has_many :items
   has_many :purchases
 
-  NAME_REGEX = /\A[ぁ-んァ-ン一-龥]/
-  KANAME_REGEX = /\A[ァ-ヶー－]+\z/
+  NAME_REGEX = /\A[ぁ-んァ-ン一-龥]/.freeze
+  KANAME_REGEX = /\A[ァ-ヶー－]+\z/.freeze
 
   with_options presence: true do
     validates :nick_name
-    validates :kanji_fam, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "Last name Full-width characters and can't be blank" }
-    validates :kanji_given, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "First name Full-width characters and can't be blank" }
-    validates :kana_fam, format: { with: /\A[ァ-ヶー－]+\z/, message: "Last name kana Full-width katakana characters and can't be blank" }
-    validates :kana_given, format: { with: /\A[ァ-ヶー－]+\z/, message: "First name kana Full-width katakana characters and can't be blank" }
+    validates :kanji_fam, format: { with: NAME_REGEX, message: "Last name Full-width characters and can't be blank" }
+    validates :kanji_given, format: { with: NAME_REGEX, message: "First name Full-width characters and can't be blank" }
+    validates :kana_fam, format: { with: KANAME_REGEX, message: "Last name kana Full-width katakana characters and can't be blank" }
+    validates :kana_given, format: { with: KANAME_REGEX, message: "First name kana Full-width katakana characters and can't be blank" }
     validates :birth
   end
 
   validates_format_of :password, with: /([0-9].*[a-zA-Z]|[a-zA-Z].*[0-9])/
-
-  # def save
-  #   # ユーザーの情報を保存し、「user」という変数に入れている
-  #   user = User.create(name: name, name_reading: name_reading, nickname: nickname)
-  #   # 住所の情報を保存
-  #   Address.create(postal_code: postal_code, prefecture: prefecture, city: city, house_number: house_number, building_name: building_name,user_id: user.id)
-  #   # 寄付金の情報を保存
-  #   Donation.create(price: price, user_id: user.id)
-  # end
 end
-
