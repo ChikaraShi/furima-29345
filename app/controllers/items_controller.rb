1814edb4697b6794ff1b4cac3b6c85c89a820a44
ChikaraShi <chikara.2000.082@gmail.com>
2020-09-23 11:58:16 +0900

    商品出品機能のビュー1

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,9 @@
 class ItemsController < ApplicationController
   def index
+    @user = User.all
+  end
+
+  def new
+    @items = Items.all
   end
 end
---
+++ app/models/items.rb
@@ -0,0 +1,23 @@
+class Item < ApplicationRecord
+  belongs_to :user
+  has_one :purchase
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+end
