7a5989e81d73d514e1a9abcda0f9b599e8b028ec
ChikaraShi <chikara.2000.082@gmail.com>
2020-09-23 14:33:36 +0900

    itemsモデルのバリデーション1

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,4 +6,13 @@ class ItemsController < ApplicationController
   def new
     @items = Items.all
   end
+
+  def create
+    Items.create(items_params)
+  end
+
+  private
+  def items_params
+    params.require(:items).permit(:image, :name, :introduce, :type_id, :codition_id, :ship_fee_id, :ship_from_id, :ship_date_id, :price, :user)
+  end
 end
--- app/models/items.rb
+++ app/models/items.rb
@@ -1,8 +1,24 @@
-class Item < ApplicationRecord
+class Items < ApplicationRecord
   belongs_to :user
   has_one :purchase
 
+  with_options presence: true do
+    validates :image
+    validates :name
+    validates :introduce
+    validates :type_id
+    validates :codition_id
+    validates :ship_fee_id
+    validates :ship_from_id
+    validates :ship_date_id
+    validates :price
+    validates :user
+  end
+  validates :price, numericality: { only_integer: true }
+  validates :price, numericality: { :less_than_or_equal_to 300, :greater_than_or_equal_to 9999999 }
 
+  validates_format_of :password, with: /([0-9].*[a-zA-Z]|[a-zzA-Z].*[0-9])/
+end
 
 
 
