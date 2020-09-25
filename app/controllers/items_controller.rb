ec4f765d2a17d1215ea5bc15adfac97d1f3d45af
ChikaraShi <chikara.2000.082@gmail.com>
2020-09-25 17:47:58 +0900

    商品一覧表示機能 fix1

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,7 @@ class ItemsController < ApplicationController
   before_action :move_to_index, except: [:index, :show]
 
   def index
-    @item = Item.all
+    @item = Item.includes(:user).order("created_at DESC")
   end
 
   def new
